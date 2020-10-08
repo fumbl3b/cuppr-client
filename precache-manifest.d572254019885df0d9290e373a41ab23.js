4d68f2724c3a06ae87b6af5b3ecfd65de0a7e634
Harrison Winkler <harryewinkler@thinkful.com>
2020-10-08 14:26:01 -0400

    Updates

---
+++ precache-manifest.d572254019885df0d9290e373a41ab23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "891c00c402a2682c17d39b95fadcc652",
+    "url": "/cuppr-client/index.html"
+  },
+  {
+    "revision": "f57c8182c5944d6da431",
+    "url": "/cuppr-client/static/css/main.2385b4f9.chunk.css"
+  },
+  {
+    "revision": "676984f8d775ac9d98fa",
+    "url": "/cuppr-client/static/js/2.464506b0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cuppr-client/static/js/2.464506b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f57c8182c5944d6da431",
+    "url": "/cuppr-client/static/js/main.4985b4d8.chunk.js"
+  },
+  {
+    "revision": "fa8506ddb47e6e160215",
+    "url": "/cuppr-client/static/js/runtime-main.4eb51300.js"
+  }
+]);
