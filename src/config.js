1b82e1ce5be1fdf66c1df20a726f53210b600a42
Harrison Winkler <harryewinkler@thinkful.com>
2020-10-15 17:44:48 -0400

    fixed typo

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: process.env.API_ENDPOINT || 'https://mighty-woodland-26432.herokuapp.com/',
+  API_ENDPOINT: process.env.API_ENDPOINT || 'https://mighty-woodland-26432.herokuapp.com',
   TOKEN_KEY: 'token key blah blah',
 }
